37bdb9943da9e9542bbe2d076ec0bf56b77901e8
fermatlee <57705561+fermatlee@users.noreply.github.com>
2019-11-14 10:32:08 +0800

    Add files via upload

---
+++ AM_vendor_delay.sql
@@ -0,0 +1,16 @@
+drop view if exists dp_bi.vendor_delay;
+create materialized view dp_bi.vendor_delay as 
+with  d as (
+		select 
+		d.order_id as order_id,
+		d.vendor_id,
+		ROUND(d.vendor_delay/60,2)::numeric as vendor_delay
+		from dp_bi.deliveries_did d
+		       ),
+	t  as (select o.order_id,
+	     	        o.is_failed_order_vendor
+	       from dp_bi.orders_oid o 
+	            )
+select d.*,t.is_failed_order_vendor
+from d
+LEFT JOIN t USING(order_id)
